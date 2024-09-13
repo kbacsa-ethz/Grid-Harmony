9991b838fc11f5fb0db3e5f54c40822dae966e37
Kiran Bacsa <kbacsa-ethz>
2024-09-13 14:13:38 +0800

    short click turns on plant, long click with drag is for connecting/disconnecting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pygame
+import time
 from sprites import BackgroundSprite, PlantSprite, CitySprite
 from geometry import segments_intersect
 from camera import Camera
@@ -30,12 +31,15 @@ num_plants, num_cities = 0, 0
 for i in range(WORLD_WIDTH):
     for j in range(WORLD_HEIGHT):
         if FIELD <= world[i, j] <= LAKE:
-            backgrounds += [BackgroundSprite(world[i, j], (i * TILE_WIDTH + TILE_WIDTH // 2, j * TILE_HEIGHT + TILE_HEIGHT // 2))]
+            backgrounds += [
+                BackgroundSprite(world[i, j], (i * TILE_WIDTH + TILE_WIDTH // 2, j * TILE_HEIGHT + TILE_HEIGHT // 2))]
         if world[i, j] == PLANT:
-            plants += [PlantSprite(random.choice(energy_options), (i * TILE_WIDTH + TILE_WIDTH // 2, j * TILE_HEIGHT + TILE_HEIGHT // 2))]
+            plants += [PlantSprite(random.choice(energy_options),
+                                   (i * TILE_WIDTH + TILE_WIDTH // 2, j * TILE_HEIGHT + TILE_HEIGHT // 2))]
             num_plants += 1
         elif world[i, j] == CITY:
-            cities += [CitySprite(random.choice(city_options), (i * TILE_WIDTH + TILE_WIDTH // 2, j * TILE_HEIGHT + TILE_HEIGHT // 2))]
+            cities += [CitySprite(random.choice(city_options),
+                                  (i * TILE_WIDTH + TILE_WIDTH // 2, j * TILE_HEIGHT + TILE_HEIGHT // 2))]
             num_cities += 1
 
 power_grid = np.zeros((num_plants, num_cities)).astype(np.bool_)
@@ -67,10 +71,11 @@ power_sound.set_volume(5.0)
 class Player:
     def __init__(self, initial_money):
         self.money = initial_money
-        
-        
+
+
 # Player
 player = Player(1000)  # Initial money
+
 
 # ... rest of the code ...
 
@@ -84,11 +89,13 @@ def build_plant(plant_type):
     else:
         print("Insufficient funds")
 
+
 def calculate_turn_costs():
     total_operational_cost = 0
     for plant in plants:
         total_operational_cost += plant.operational_cost
     player.money -= total_operational_cost
+
 
 def calculate_income():
     income = sum(powered_cities) * 10  # Simple income model
@@ -108,41 +115,51 @@ while running:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             # lock/unlock plant if clicked on
             if event.button == LEFT:
-                for i, plant in enumerate(plants):
-                    event_pos_in_mask = event.pos[0] - plant.relative_rect.x, event.pos[1] - plant.relative_rect.y
-                    if plant.relative_rect.collidepoint(event.pos) and plant.mask.get_at(event_pos_in_mask):
-                        activated_plants[i] = not activated_plants[i]
                 # Start drawing the line
+                click_start_time = time.time()
                 start_pos = event.pos
-                draw_wire = True
-                activate_sound.play()
             elif event.button == SCROLL_UP:  # Mouse wheel up
                 camera.update_zoom(camera.zoom_step, pygame.mouse.get_pos())
             elif event.button == SCROLL_DOWN:  # Mouse wheel down
                 camera.update_zoom(-camera.zoom_step, pygame.mouse.get_pos())
         elif event.type == pygame.MOUSEBUTTONUP and event.button == LEFT:
+            click_duration = time.time() - click_start_time
             # Stop drawing the line and finalize it
             end_pos = event.pos
-            draw_wire = False
-        elif event.type == pygame.MOUSEMOTION and draw_wire:
-            # Update the end position of the line while dragging
-            end_pos = event.pos
+            if start_pos is None:
+                start_pos = end_pos
+            distance = ((end_pos[0] - start_pos[0]) ** 2 +
+                        (end_pos[1] - start_pos[1]) ** 2) ** 0.5
+
+            if draw_wire:
+                draw_wire = False
+            elif click_duration <= CLICK_THRESHOLD and distance < DRAG_THRESHOLD:
+                for i, plant in enumerate(plants):
+                    event_pos_in_mask = event.pos[0] - plant.relative_rect.x, event.pos[1] - plant.relative_rect.y
+                    if plant.relative_rect.collidepoint(event.pos) and plant.mask.get_at(event_pos_in_mask):
+                        activated_plants[i] = not activated_plants[i]
+                        activate_sound.play()
+        elif event.type == pygame.MOUSEMOTION:
+            if pygame.mouse.get_pressed()[0]:  # Left mouse button is held down
+                # Update the end position of the line while dragging
+                end_pos = event.pos
+                if not draw_wire:
+                    distance = ((end_pos[0] - start_pos[0]) ** 2 +
+                                (end_pos[1] - start_pos[1]) ** 2) ** 0.5
+                    if distance >= drag_threshold:
+                        draw_wire = True
 
     # GAME LOGIC
 
-    # prevent point to carry over
-    if not draw_wire:
-        start_pos = None
-        end_pos = None
-
     # Update connections (only while mouse is used)
-    if start_pos and end_pos:
+    if draw_wire:
         # Adding connections
         # iterate all cities for all plants. if the wire connects them and they are not already connected, then connect them
         for i, plant in enumerate(plants):
             start_pos_in_mask = start_pos[0] - plant.relative_rect.x, start_pos[1] - plant.relative_rect.y
             # check is plant is unlocked, is the click in the rectangle, is the click pixel perfect
-            if activated_plants[i] and plant.relative_rect.collidepoint(start_pos) and plant.mask.get_at(start_pos_in_mask):
+            if activated_plants[i] and plant.relative_rect.collidepoint(start_pos) and plant.mask.get_at(
+                    start_pos_in_mask):
                 for j, city in enumerate(cities):
                     end_pos_in_mask = end_pos[0] - city.relative_rect.x, end_pos[1] - city.relative_rect.y
                     if city.relative_rect.collidepoint(end_pos) and city.mask.get_at(end_pos_in_mask):
@@ -193,7 +210,8 @@ while running:
     for i in range(num_plants):
         for j in range(num_cities):
             if power_grid[i, j]:
-                pygame.draw.line(screen, YELLOW, plants[i].relative_rect.center, cities[j].relative_rect.center, WIRE_WIDTH)
+                pygame.draw.line(screen, YELLOW, plants[i].relative_rect.center, cities[j].relative_rect.center,
+                                 WIRE_WIDTH)
 
     # Draw plant and city sprites
     for sprite in all_plants:
@@ -225,7 +243,6 @@ while running:
 
     # update the display
     pygame.display.flip()
-
 
 # End game celebration
 pygame.mixer.music.stop()
